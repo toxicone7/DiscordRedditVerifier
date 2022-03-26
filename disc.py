1adc0e677ec2554e5aaeb1452a29ef797319e5aa
ntebis <ntebis@users.noreply.github.com>
2022-03-26 18:58:45 +1030

    Added Changes to disc.py and reddit.py
    reddit.py
    - Implemented asyncpraw.
    - removed login (implemented in disc.py)
    - now used more like a library.
    - renamed search_user_contraints to search_user_stats
    - commented out the java enums and implemented similar functionality with a boolean dict
    
    disc.py
    - commented out the on_message function
    - implemented a on add reaction function

--- disc.py
+++ disc.py
@@ -1,8 +1,18 @@
 import discord
-from reddit import search_user
+from discord.utils import get
+from reddit import search_user, read_csv, search_user_stats
+import re
+
 from enums import ReturnType
 
+
+import asyncpraw
+
 client = discord.Client()
+client_id, client_secret, password, user_agent, username = read_csv()
+
+
+
 
 with open('token.txt', 'r') as t:
     token = t.read()
@@ -14,39 +24,111 @@ async def on_ready():
     print("Log on as {0.user}".format(client))
 
 
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+#     if str(message.channel).lower() == 'roles':
+#         discord_sent_username = str(message.content).lower()
+#         user = message.author
+#         # Search Messages through reddit for Discord Sent User
+#         returnType = search_user(discord_sent_username)
+#         if returnType == ReturnType.Accepted:
+#             # Assign the id of the role
+#             role = discord.Role
+#             role.id = 956182931485900800
+#             # Give the user the role
+#             print("Verified user: ", user, " with user id: ", user.id)
+#             await user.add_roles(role)
+#             discord_message = "Verified user: " + str(user) + " with user id: " + str(user.id)
+#             await message.channel.send(discord_message)
+#         elif returnType == ReturnType.Declined:
+#             print("Not Validated user: ", user, " with user id: ", user.id)
+#             discord_message = "Sorry but it seems that the user: " + str(user) + " with user id: " + str(
+#                 user.id) + " can't be verified."
+#             await message.channel.send(discord_message)
+#             await message.channel.send("Try again after you get some activity on /r/greece.")
+#         elif returnType == ReturnType.NoSuchUsername:
+#             print("There's no message on reddit from the user: ", discord_sent_username, " and subject 'Discord'")
+#             discord_message = "There's no message on reddit from the user: " + str(
+#                 discord_sent_username) + " and subject 'Discord'"
+#             await message.channel.send(discord_message)
+#         elif returnType == ReturnType.AlreadyInDiscord:
+#             print("The user: ", user, " is already on the server.")
+#             discord_message = "The user: " + str(user) + " is already on the server."
+#             await message.channel.send(discord_message)
+
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if str(message.channel).lower() == 'roles':
-        discord_sent_username = str(message.content).lower()
-        user = message.author
-        # Search Messages through reddit for Discord Sent User
-        returnType = search_user(discord_sent_username)
-        if returnType == ReturnType.Accepted:
-            # Assign the id of the role
-            role = discord.Role
-            role.id = 956182931485900800
-            # Give the user the role
-            print("Verified user: ", user, " with user id: ", user.id)
-            await user.add_roles(role)
-            discord_message = "Verified user: " + str(user) + " with user id: " + str(user.id)
-            await message.channel.send(discord_message)
-        elif returnType == ReturnType.Declined:
-            print("Not Validated user: ", user, " with user id: ", user.id)
-            discord_message = "Sorry but it seems that the user: " + str(user) + " with user id: " + str(
-                user.id) + " can't be verified."
-            await message.channel.send(discord_message)
-            await message.channel.send("Try again after you get some activity on /r/greece.")
-        elif returnType == ReturnType.NoSuchUsername:
-            print("There's no message on reddit from the user: ", discord_sent_username, " and subject 'Discord'")
-            discord_message = "There's no message on reddit from the user: " + str(
-                discord_sent_username) + " and subject 'Discord'"
-            await message.channel.send(discord_message)
-        elif returnType == ReturnType.AlreadyInDiscord:
-            print("The user: ", user, " is already on the server.")
-            discord_message = "The user: " + str(user) + " is already on the server."
-            await message.channel.send(discord_message)
+async def on_raw_reaction_add(payload):
+
+    channel = client.get_channel(957100463369617470)
+    postchannel = client.get_channel(956182853006270464)
+    message_id = 957032030036713575
+    admin_role = client.get_guild(956182780251873311).get_role(956182931485900800)
+
+    # print(payload)
+    if payload.message_id == message_id:
+        print('reacted to message')
+        reddit = asyncpraw.Reddit(
+                    client_id=client_id,
+                    client_secret=client_secret,
+                    password=password,
+                    user_agent=user_agent,
+                    username=username,
+                )
+        async for message in reddit.inbox.unread():
+            if str(message.subject).lower() == 'discord':
+
+                user = message.author
+                # TODO:Check db for existing account
+                # parsed_disc_user = re.findall(".*#\d{4}", message.body)
+                parsed_disc_user = message.body.strip().split('#')
+                print('parsed: ', parsed_disc_user)
+                print('payload: ', payload.member.name)
+
+                if len(parsed_disc_user) > 0:
+                    # Check if the user has reacted
+                    if payload.member.name.lower() == parsed_disc_user[0].lower():
+
+                        print(payload.member.name)
+                        print(parsed_disc_user)
+                        await user.load()
+                        summary = await search_user_stats(user)
+                        summary.append(f"Name on Discord: **{'#'.join(parsed_disc_user)}**")
+                        summary.append(f"__Options for *{'#'.join(parsed_disc_user)}*:__ \n✅ - Verify\n⁉️- Verify and probate\n⛔ - No Verification")
+                        msg = await postchannel.send('\n'.join(summary))
+                        await msg.add_reaction(emoji='✅')
+                        await msg.add_reaction(emoji='⁉️')
+                        await msg.add_reaction(emoji='⛔')
+
+                        def check(reaction, user):
+                            for role in user.roles:
+                                if role == admin_role:
+                                    return True
+
+                            return False
+
+                        try:
+                            reaction, react_user = await client.wait_for('reaction_add', check=check)
+                            print(reaction)
+                            if reaction == '✅': #add role
+                                continue
+                            elif reaction == '⁉️': #add_role and probate
+                                continue
+                            elif reaction == '⛔': #deny and send message
+                                continue
+                        except:
+                            continue
+
+
+
+
+
+
+
+
 
 
 client.run(token)
+
+
--- reddit.py
+++ reddit.py
@@ -1,5 +1,5 @@
-import praw
-#import asyncpraw
+# import praw
+import asyncpraw
 from datetime import datetime
 import pandas as pd
 from enums import ReturnType
@@ -23,34 +23,41 @@ def read_csv():
         print(e)
 
 
-def login():
-    # Read authentication data from the .csv file
-    client_id, client_secret, password, user_agent, username = read_csv()
-    try:
-        reddit = praw.Reddit(
-            client_id=client_id,
-            client_secret=client_secret,
-            password=password,
-            user_agent=user_agent,
-            username=username,
-        )
-        return reddit
-    except Exception as e:
-        print("Error while trying to login to reddit.")
-        print(e)
-        return None
+client_id, client_secret, password, user_agent, username = read_csv()
 
 
-def stats_view(user_dict,six_mos ,mode):
+# def login():
+#     # Read authentication data from the .csv file
+#     client_id, client_secret, password, user_agent, username = read_csv()
+#     try:
+#         reddit = praw.Reddit(
+#             client_id=client_id,
+#             client_secret=client_secret,
+#             password=password,
+#             user_agent=user_agent,
+#             username=username,
+#         )
+#         return reddit
+#     except Exception as e:
+#         print("Error while trying to login to reddit.")
+#         print(e)
+#         return None
+
+
+def stats_view(user_dict, six_mos, mode):
     keys = user_dict.keys()
     vals = user_dict.values()
 
     v, k = zip(*sorted((zip(vals, keys)), reverse=True))
 
     total = sum(vals)
-    print('From the last ', total, mode)
-    print('{:<20}| Percentage | Number | 6 Months'.format("Subreddit"))
-    print('-----------------------------------------------------')
+    stats = "```"
+    # print('From the last ', total, mode)
+    # print('{:<20}| Percentage | Number | 6 Months'.format("Subreddit"))
+    # print('-----------------------------------------------------')
+    stats += f'From the last {total} {mode}\n'
+    stats += '{:<20}| Percentage | Number | 6 Months\n'.format("Subreddit")
+    stats += '-----------------------------------------------------\n'
     other_count = 0
     other_6_mos = 0
     for i in range(len(vals)):
@@ -58,89 +65,119 @@ def stats_view(user_dict,six_mos ,mode):
             other_count += v[i]
             other_6_mos += six_mos[k[i]]
             continue
-        print("{:<20}| {:<10} | {:<6} | {} ".format(k[i], str(v[i]/total*100)+"%", v[i], six_mos[k[i]]))
+        # print("{:<20}| {:<10} | {:<6} | {} ".format(k[i], str(v[i]/total*100)+"%", v[i], six_mos[k[i]]))
+        stats += "{:<20}| {:<10} | {:<6} | {} \n".format(k[i], str(v[i] / total * 100) + "%", v[i], six_mos[k[i]])
 
     if len(vals) > 10:
-        print("{:<20}| {:<10} | {:<6} | {} ".format('Other Subreddits', str(other_count/ total * 100) + "%", other_count, other_6_mos))
+        # print("{:<20}| {:<10} | {:<6} | {} ".format('Other Subreddits', str(other_count/ total * 100) + "%", other_count, other_6_mos))
+        stats += "{:<20}| {:<10} | {:<6} | {} \n".format('Other Subreddits', str(other_count / total * 100) + "%",
+                                                         other_count, other_6_mos)
+    stats += '```'
+
+    return stats
 
 
-def read_inbox(reddit, discord_sent_username):
-    for message in reddit.inbox.unread():
-        if str(message.subject).lower() == 'discord':
-            user = message.author
-            ## TO-DO ##
-            # Check db for existing account
+# def read_inbox(reddit, discord_sent_username):
+#     reddit = asyncpraw.Reddit(
+#         client_id=client_id,
+#         client_secret=client_secret,
+#         password=password,
+#         user_agent=user_agent,
+#         username=username,
+#     )
+#
+#
+#     for message in reddit.inbox.unread():
+#         if str(message.subject).lower() == 'discord':
+#             user = message.author
+#             ## TO-DO ##
+#             # Check db for existing account
+#
+#             # Checks that the reddit user is equal to the message from discord
+#             if str(user).lower() == discord_sent_username:
+#                 print("Now searching user: ", user)
+#                 message.mark_read()
+#                 # Search the constraints
+#                 return search_user_constraints(user)
+#             else:
+#                 # If there usernames sent don't match and don't mark message as read
+#                 return ReturnType.NoSuchUsername
+#
+#         else:
+#             # Mark message as read and return the NoSuchUsername type
+#             message.mark_read()
+#             return ReturnType.NoSuchUsername
+#
+#     # If code reaches here it means no message with subject 'Discord' is found
+#     return ReturnType.NoSuchUsername
 
-            # Checks that the reddit user is equal to the message from discord
-            if str(user).lower() == discord_sent_username:
-                print("Now searching user: ", user)
-                message.mark_read()
-                # Search the constraints
-                return search_user_constraints(user)
-            else:
-                # If there usernames sent don't match and don't mark message as read
-                return ReturnType.NoSuchUsername
 
-        else:
-            # Mark message as read and return the NoSuchUsername type
-            message.mark_read()
-            return ReturnType.NoSuchUsername
-
-    # If code reaches here it means no message with subject 'Discord' is found
-    return ReturnType.NoSuchUsername
-
-
-def search_user_constraints(user):
+async def search_user_stats(user):
+    # print(f'Redditor: {user.name}')
+    # print(f'Suspended?: {user.is_suspended}')
     # Getting total karma
     total_karma = int(user.link_karma) + int(user.comment_karma)
-
+    summary = []
+    determination_flags = defaultdict(int)
+    summary.append(f"__**Summary for user /r/{user.name}**__.\n<https://reddit.com/r/{user.name}>")
     # Checking total karma constraint
-    if total_karma > 1000:
-        print("More than 1k karma")
-    else:
-        print("Less than 1k karma")
-        return ReturnType.Declined
+    # if total_karma > 1000:
+    #     print("More than 1k karma")
+    # else:
+    #     print("Less than 1k karma")
+    summary.append(f"The user has {total_karma} karma.")
+
+    print(summary[-1])  # debug
+
+    determination_flags["karma"] = int(total_karma > 1000)
 
     # Checking account created more than 6 months ago constraint
     # 180days = 6 months
     date_created = datetime.fromtimestamp(user.created_utc)
 
     if abs(datetime.now() - date_created).days > 180:
-        print("Account older than 6 months")
+        summary.append("The account is older than 6 months")
     else:
-        print("Account not older than 6 months")
-        return ReturnType.Declined
+        print("The account is **NOT** older than 6 months")
+        # return ReturnType.Declined
+
+    print(summary[-1])
+    determination_flags['age'] = int(abs(datetime.now() - date_created).days > 180)
 
     # Get comments from 't5_2qh8i' -> /r/greece
-    comments = user.new(limit=200)
-    print(comments)
+    # comments = user.comments.new(limit=200)
+    # print(comments)
     comment_count = defaultdict(int)
     comments_6mos = defaultdict(int)
-    for comment in comments:
+    async for comment in user.comments.new(limit=200):
         comment_count[str(comment.subreddit.display_name.lower())] += 1
 
         if abs(datetime.fromtimestamp(comment.created_utc) - datetime.now()).days < 180:
-            comments_6mos[comment.subreddit.display_name] += 1
+            comments_6mos[str(comment.subreddit.display_name.lower())] += 1
 
-    print(comment_count)
-    stats_view(comment_count, comments_6mos, "Comments")
+    # print(comment_count)
+    summary.append(stats_view(comment_count, comments_6mos, "Comments"))
+    print(summary[-1])
 
     submission_count = defaultdict(int)
-    submissions = user.submissions.new(limit=200)
+    # submissions = user.submissions.new(limit=200)
     submission_6mos = defaultdict(int)
-    for submission in submissions:
+
+    async for submission in user.submissions.new(limit=200):
         submission_count[str(submission.subreddit.display_name).lower()] += 1
         if abs(datetime.fromtimestamp(submission.created_utc) - datetime.now()).days < 180:
-            submission_6mos[submission.subreddit.display_name] += 1
+            submission_6mos[str(submission.subreddit.display_name).lower()] += 1
 
-    stats_view(submission_count, submission_6mos, "Submissions")  # provide a percentage summary of the user
-
+    summary.append(stats_view(submission_count, submission_6mos, "Submissions"))
+    print(summary[-1])
 
     total_comments = comment_count['greece']
     total_submissions = submission_count['greece']
 
-    print("Total comments from /r/greece: ", total_comments)
-    print("Total submissions from /r/greece: ", total_submissions)
+    summary.append(f'Total comments from /r/greece: {total_comments}')
+    summary.append(f'Total submissions from /r/greece: {total_submissions}')
+    print(summary[-1])
+    print(summary[-2])
 
     # TODO: check if user is banned
 
@@ -153,10 +190,11 @@ def search_user_constraints(user):
 
     # TODO: Blacklist. Read from a file that has a blacklisted subreddit on each line. If in blacklist view participation in table
 
-    if total_comments < 20 or total_submissions < 5:
-        return ReturnType.Declined
+    # if total_comments < 20 or total_submissions < 5:
+    #     return ReturnType.Declined
 
-    return ReturnType.Accepted
+    determination_flags['participation'] = int(total_comments < 20 or total_submissions < 5)
+    return summary
 
 
 def search_user(discord_sent_username):
